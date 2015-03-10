546536bea0d6447ca4d159de36993db3c0b410fb
Nanqiao Deng <nqdeng@gmail.com>
2015-03-10 12:12:17 +0800

    format source code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var PATTERN_RULE = /^([^\/]+?)?(\/.*?)?$/;
 
 /**
@@ -7,9 +9,9 @@ var PATTERN_RULE = /^([^\/]+?)?(\/.*?)?$/;
  * @return {boolean}
  */
 function match(expect, input) {
-	return expect.every(function (value, index) {
-		return value === input[index];
-	});
+    return expect.every(function (value, index) {
+        return value === input[index];
+    });
 }
 
 /**
@@ -20,15 +22,15 @@ function match(expect, input) {
  * @return {Array}
  */
 function split(str, delimiter, reverse) {
-	var parts = str.split(delimiter).filter(function (value) {
-		return value;
-	})
-	
-	if (delimiter === '.') {
-		parts.reverse();
-	}
-	
-	return parts;
+    var parts = str.split(delimiter).filter(function (value) {
+        return value;
+    })
+    
+    if (delimiter === '.') {
+        parts.reverse();
+    }
+    
+    return parts;
 }
 
 /**
@@ -37,39 +39,39 @@ function split(str, delimiter, reverse) {
  * @return {Function}
  */
 module.exports = function (rule) {
-	var re = (rule || '').match(PATTERN_RULE);
-	
-	// "www.example.com" => [ "com", "example", "www" ]
-	var	hostname = split(re[1] || '', '.', true);
-	
-	// "/foo/bar/baz" => [ "foo", "bar", "baz" ]
-	var pathname = split(re[2] || '', '/');
-	
-	// base should be "", "/foo", "/foo/bar", etc..
-	var	base = pathname.length === 0 ? '' : '/' + pathname.join('/');
-	
-	return function *(next) {
-		var req = this.request,
-			originalBase = req.base;
+    var re = (rule || '').match(PATTERN_RULE);
+    
+    // "www.example.com" => [ "com", "example", "www" ]
+    var hostname = split(re[1] || '', '.', true);
+    
+    // "/foo/bar/baz" => [ "foo", "bar", "baz" ]
+    var pathname = split(re[2] || '', '/');
+    
+    // base should be "", "/foo", "/foo/bar", etc..
+    var base = pathname.length === 0 ? '' : '/' + pathname.join('/');
+    
+    return function *(next) {
+        var req = this.request,
+            originalBase = req.base;
 
-		// Match hostname at first.
-		if (!match(hostname, split(req.hostname, '.', true))) {
-			return;
-		}
+        // Match hostname at first.
+        if (!match(hostname, split(req.hostname, '.', true))) {
+            return;
+        }
 
-		// Match pathname at last.
-		if (!match(pathname, split(req.pathname, '/'))) {
-			return;
-		}
-		
-		// Trim the base so the sub middlewares won't cara about the mount path.
-		req.url(req.pathname.replace(base, '') || '/');
-		req.base = base;
-		
-		yield next;
+        // Match pathname at last.
+        if (!match(pathname, split(req.pathname, '/'))) {
+            return;
+        }
+        
+        // Trim the base so the sub middlewares won't cara about the mount path.
+        req.url(req.pathname.replace(base, '') || '/');
+        req.base = base;
+        
+        yield next;
 
-		// Restore the original pathname.
-		req.base = originalBase;
-		req.url(base + req.pathname);
-	};
+        // Restore the original pathname.
+        req.base = originalBase;
+        req.url(base + req.pathname);
+    };
 };
--- test/mount.js
+++ test/mount.js
@@ -1,198 +1,200 @@
+'use strict';
+
 var http = require('http');
 var mount = require('..');
 var request = require('supertest');
 var tianma = require('tianma');
 
 function createApp() {
-	var app = tianma();
-	var	server = http.createServer(app.run);
-		
-	app.server = server;
-	
-	return app;
+    var app = tianma();
+    var server = http.createServer(app.run);
+        
+    app.server = server;
+    
+    return app;
 }
 
 describe('mount(pathname)', function () {
-	function createServer(pathname) {
-		var app = createApp();
-		
-		app.use(mount(pathname)).then
-			.use(function *(next) {
-				this.response.data([
-					this.request.base,
-					'\n',
-					this.request.pathname
-				]);
-			});
-			
-		return app.server;
-	}
-	
-	it('should mount at /', function (done) {
-		request(createServer('/'))
-			.get('/foo/bar')
-			.expect('\n/foo/bar')
-			.end(done);
-	});
-	
-	it('should mount at /foo', function (done) {
-		request(createServer('/foo'))
-			.get('/foo/bar/')
-			.expect('/foo\n/bar/')
-			.end(done);
-	});
-	
-	it('should mount at /foo/bar', function (done) {
-		request(createServer('/foo/bar'))
-			.get('/foo/bar/')
-			.expect('/foo/bar\n/')
-			.end(done);
-	});
-	
-	it('should reject the unmatched pathname', function (done) {
-		request(createServer('/foo'))
-			.get('/bar')
-			.expect('')
-			.end(done);
-	});
+    function createServer(pathname) {
+        var app = createApp();
+        
+        app.use(mount(pathname)).then
+            .use(function *(next) {
+                this.response.data([
+                    this.request.base,
+                    '\n',
+                    this.request.pathname
+                ]);
+            });
+            
+        return app.server;
+    }
+    
+    it('should mount at /', function (done) {
+        request(createServer('/'))
+            .get('/foo/bar')
+            .expect('\n/foo/bar')
+            .end(done);
+    });
+    
+    it('should mount at /foo', function (done) {
+        request(createServer('/foo'))
+            .get('/foo/bar/')
+            .expect('/foo\n/bar/')
+            .end(done);
+    });
+    
+    it('should mount at /foo/bar', function (done) {
+        request(createServer('/foo/bar'))
+            .get('/foo/bar/')
+            .expect('/foo/bar\n/')
+            .end(done);
+    });
+    
+    it('should reject the unmatched pathname', function (done) {
+        request(createServer('/foo'))
+            .get('/bar')
+            .expect('')
+            .end(done);
+    });
 });
 
 describe('mount(hostname)', function () {
-	function createServer(hostname) {
-		var app = createApp();
-		
-		app.use(mount(hostname)).then
-			.use(function *(next) {
-				this.response.data(this.request.hostname);
-			});
-			
-		return app.server;
-	}
+    function createServer(hostname) {
+        var app = createApp();
+        
+        app.use(mount(hostname)).then
+            .use(function *(next) {
+                this.response.data(this.request.hostname);
+            });
+            
+        return app.server;
+    }
 
-	it('should mount at localhost', function (done) {
-		request(createServer('localhost'))
-			.get('/')
-			.set('Host', 'localhost')
-			.expect('localhost')
-			.end(done);
-	});
-	
-	it('should mount at example.com', function (done) {
-		request(createServer('example.com'))
-			.get('/')
-			.set('Host', 'example.com')
-			.expect('example.com')
-			.end(done);
-	});
-	
-	it('should accept sub domain', function (done) {
-		request(createServer('example.com'))
-			.get('/')
-			.set('Host', 'www.example.com')
-			.expect('www.example.com')
-			.end(done);
-	});
-	
-	it('should reject the unmatched domain', function (done) {
-		request(createServer('localhost'))
-			.get('/')
-			.set('Host', 'www.example.com')
-			.expect('')
-			.end(done);
-	});
+    it('should mount at localhost', function (done) {
+        request(createServer('localhost'))
+            .get('/')
+            .set('Host', 'localhost')
+            .expect('localhost')
+            .end(done);
+    });
+    
+    it('should mount at example.com', function (done) {
+        request(createServer('example.com'))
+            .get('/')
+            .set('Host', 'example.com')
+            .expect('example.com')
+            .end(done);
+    });
+    
+    it('should accept sub domain', function (done) {
+        request(createServer('example.com'))
+            .get('/')
+            .set('Host', 'www.example.com')
+            .expect('www.example.com')
+            .end(done);
+    });
+    
+    it('should reject the unmatched domain', function (done) {
+        request(createServer('localhost'))
+            .get('/')
+            .set('Host', 'www.example.com')
+            .expect('')
+            .end(done);
+    });
 });
 
 describe('mount(hostname/pathname)', function () {
-	function createServer(rule) {
-		var app = createApp();
-		
-		app.use(mount(rule)).then
-			.use(function *(next) {
-				this.response.data([
-					this.request.hostname,
-					'\n',
-					this.request.base,
-					'\n',
-					this.request.pathname
-				]);
-			});
-			
-		return app.server;
-	}
+    function createServer(rule) {
+        var app = createApp();
+        
+        app.use(mount(rule)).then
+            .use(function *(next) {
+                this.response.data([
+                    this.request.hostname,
+                    '\n',
+                    this.request.base,
+                    '\n',
+                    this.request.pathname
+                ]);
+            });
+            
+        return app.server;
+    }
 
-	it('should mount at ""', function (done) {
-		request(createServer())
-			.get('/foo/bar')
-			.set('Host', 'localhost')
-			.expect('localhost\n\n/foo/bar')
-			.end(done);
-	});
-	
-	it('should mount at "localhost/foo/"', function (done) {
-		request(createServer('localhost/foo/'))
-			.get('/foo/bar')
-			.set('Host', 'localhost')
-			.expect('localhost\n/foo\n/bar')
-			.end(done);
-	});
-	
-	it('should reject the unmatched domain', function (done) {
-		request(createServer('localhost/foo/'))
-			.get('/')
-			.set('Host', 'my-localhost')
-			.expect('')
-			.end(done);
-	});
-	
-	it('should reject the unmatched pathname', function (done) {
-		request(createServer('localhost/foo/'))
-			.get('/foo-bar')
-			.set('Host', 'localhost')
-			.expect('')
-			.end(done);
-	});
+    it('should mount at ""', function (done) {
+        request(createServer())
+            .get('/foo/bar')
+            .set('Host', 'localhost')
+            .expect('localhost\n\n/foo/bar')
+            .end(done);
+    });
+    
+    it('should mount at "localhost/foo/"', function (done) {
+        request(createServer('localhost/foo/'))
+            .get('/foo/bar')
+            .set('Host', 'localhost')
+            .expect('localhost\n/foo\n/bar')
+            .end(done);
+    });
+    
+    it('should reject the unmatched domain', function (done) {
+        request(createServer('localhost/foo/'))
+            .get('/')
+            .set('Host', 'my-localhost')
+            .expect('')
+            .end(done);
+    });
+    
+    it('should reject the unmatched pathname', function (done) {
+        request(createServer('localhost/foo/'))
+            .get('/foo-bar')
+            .set('Host', 'localhost')
+            .expect('')
+            .end(done);
+    });
 });
 
 describe('url revert', function () {
-	it('should restore the original url in bubble up phase', function (done) {
-		var app = createApp();
-		
-		app
-			.use(function *(next) {
-				yield next;
-				this.response.data(this.request.pathname + this.response.data());
-			})
-			.use(mount('/foo')).then
-				.use(function *(next) {
-					this.response.data(this.request.base);
-				})
-				.end;
-	
-		request(app.server)
-			.get('/foo/bar')
-			.expect('/foo/bar/foo')
-			.end(done);
-	});
-	
-	it('should behave correct in multi-level mounting', function (done) {
-		var app = createApp();
-		
-		app
-			.use(mount('/foo')).then
-				.use(function *(next) {
-					yield next;
-					this.response.data(this.request.base + this.response.data());
-				})
-				.use(mount('/bar')).then
-					.use(function *(next) {
-						this.response.data(this.request.base);
-					})
-					.end
-				.end;
-	
-		request(app.server)
-			.get('/foo/bar/baz')
-			.expect('/foo/bar')
-			.end(done);
-	});
+    it('should restore the original url in bubble up phase', function (done) {
+        var app = createApp();
+        
+        app
+            .use(function *(next) {
+                yield next;
+                this.response.data(this.request.pathname + this.response.data());
+            })
+            .use(mount('/foo')).then
+                .use(function *(next) {
+                    this.response.data(this.request.base);
+                })
+                .end;
+    
+        request(app.server)
+            .get('/foo/bar')
+            .expect('/foo/bar/foo')
+            .end(done);
+    });
+    
+    it('should behave correct in multi-level mounting', function (done) {
+        var app = createApp();
+        
+        app
+            .use(mount('/foo')).then
+                .use(function *(next) {
+                    yield next;
+                    this.response.data(this.request.base + this.response.data());
+                })
+                .use(mount('/bar')).then
+                    .use(function *(next) {
+                        this.response.data(this.request.base);
+                    })
+                    .end
+                .end;
+    
+        request(app.server)
+            .get('/foo/bar/baz')
+            .expect('/foo/bar')
+            .end(done);
+    });
 });
